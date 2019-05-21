2eb99699f09aae34360af985f75eff7008b918e7
Arman Kirakosyan <arkirakosyan@gmail.com>
2019-05-21 21:12:05 +0400

    uuuu

--- HackerRank/Problems/Other/MyTree.cs
+++ HackerRank/Problems/Other/MyTree.cs
@@ -11,9 +11,21 @@ namespace HackerRank.Problems.Other
         public override void MainRun()
         {
             MyTree tree = new MyTree();
-            tree.AddRange(new int[] { 5, 6, 4, 7, 3, 8, 2 });
+            //tree.AddRange(new int[] { 5, 6, 4, 7, 3, 8, 2 });
+            tree.AddRange(new int[] { 5,3, 7, 4, 2, 6, 8 });
 
-            tree.InOrderTeraversal();
+            //tree.AddRange(new int[] { 4,7,8,3,6,5,2 });
+
+
+            //tree.InOrderTeraversal();
+            //Console.WriteLine();
+            //tree.PostOrderTeraversal();
+            tree.LevelOrderTraversal();
+            Console.WriteLine();
+
+            //tree.PreOrderTeraversal();
+
+            Console.WriteLine(tree.CalcTreeHeight());
         }
     }
 
@@ -73,9 +85,67 @@ namespace HackerRank.Problems.Other
         public void InOrderTeraversal()
         {
             if (Left != null) Left.InOrderTeraversal();
-            Console.WriteLine(Value.ToString() + " ");
+            Console.Write(Value.ToString() + " ");
             if (Right != null) Right.InOrderTeraversal();
+        }
 
+        public void PreOrderTeraversal()
+        {
+            Console.Write(Value.ToString() + " ");
+            if (Left != null) Left.InOrderTeraversal();
+            if (Right != null) Right.InOrderTeraversal();
+        }
+
+        public void PostOrderTeraversal()
+        {
+            if (Left != null) Left.InOrderTeraversal();
+            if (Right != null) Right.InOrderTeraversal();
+            Console.Write(Value.ToString() + " ");
+        }
+
+        public void LevelOrderTraversal()
+        {
+            Stack<MyTree> nodes = new Stack<MyTree>();
+            Queue<MyTree> nodes1 = new Queue<MyTree>();
+
+            nodes.Push(this);
+
+            bool traversalFinished = false;
+            bool orderLeftToRight = true;
+            while (!traversalFinished)
+            {
+                while (nodes.Count > 0)
+                {
+                    MyTree myTree = nodes.Pop();
+
+                    Console.Write(myTree.Value.ToString() + " ");
+
+
+                    if (myTree.Left != null) nodes1.Enqueue(myTree.Left);
+                    if (myTree.Right != null) nodes1.Enqueue(myTree.Right);
+
+                }
+
+                while (nodes1.Count > 0)
+                {
+                    nodes.Push(nodes1.Dequeue());
+                }
+              
+                Console.WriteLine(" ");
+               // orderLeftToRight = !orderLeftToRight;
+                nodes1.Clear();
+                if (nodes.Count == 0) traversalFinished = true;
+            }
+
+        }
+        public int CalcTreeHeight()
+        {
+            if (Left == null && Right == null) return 1;
+
+            if (Left == null && Right != null) return Right.CalcTreeHeight() + 1;
+            if (Left != null && Right == null) return Left.CalcTreeHeight() + 1;
+
+            return Math.Max(Left.CalcTreeHeight(), Right.CalcTreeHeight()) + 1;
         }
     }
 }
