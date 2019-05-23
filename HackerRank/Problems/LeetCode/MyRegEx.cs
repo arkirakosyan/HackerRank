923b01b09e7169a505f6bbfaf0b60d1d59af4d69
Arman Kirakosyan <arkirakosyan@gmail.com>
2019-05-24 02:36:22 +0400

    asd

--- HackerRank/Problems/LeetCode/MyRegEx.cs
+++ HackerRank/Problems/LeetCode/MyRegEx.cs
@@ -10,72 +10,111 @@ namespace HackerRank.Problems.LeetCode
     {
         public override void MainRun()
         {
-            Print(IsMatch("aab", "c*a*b"));
+            Print(IsMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*"));
+            Print(IsMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*"));
+            Print(IsMatch("aa", "a*c*"));
+
+            Print(IsMatch("bbab", "a*.*b"));
+
+            Print(IsMatch("a", "ac*"));
+
+            Print(IsMatch("a", "ab*a"));
+
+            Print(IsMatch("aa", "a*"));
+
+            Print(IsMatch("aaa", "a.a"));
+
+            Print(IsMatch("mississippi", "."));
+            Print(IsMatch("pi", "p*."));
+            //Print(IsMatch("ii", "s*."));
+
+
+            Print(IsMatch("aaaaab", ".a..aa*b"));
+            Print(IsMatch("ab", "a*b"));
+
+            Print(IsMatch("a", "ab*"));
+            Print(IsMatch("a", ".*"));
+            Print(IsMatch("a", ".*..a*"));
         }
 
         public bool IsMatch(string s, string p)
         {
+            if (s == p || (s.Length == 1 && p == ".")) return true;
+
             int sIndex = 0;
-
-            for (int i = 0; i < p.Length; i++)
+            int i = 0;
+            for (i = 0; i < p.Length; i++)
             {
-                if (p[i] >= 'a' && p[i] <= 'z')
+                if (i < p.Length - 1 && p[i + 1] == '*')
                 {
-                    if (i < p.Length - 1 && p[i + 1] == '*')
+                    do
                     {
-                        if (i < p.Length - 2)
-                        {
-                            do
-                            {
-                                if (IsMatch(s.Substring(sIndex), p.Substring(i + 2)))
-                                {
-                                    return true;
-                                }
-                            }
-                            while (s[sIndex++] == s[i]);
-                          
-                            return false;
-                        }
-                        else
+                        if (sIndex <= s.Length && IsMatch(s.Substring(sIndex), p.Substring(i + 2)))
                         {
                             return true;
                         }
                     }
-                    else
-                    {
-                        if (s[sIndex] != p[i] || (++sIndex >= s.Length && i + 1 < p.Length)) return false;
-                    }
+                    while (sIndex < s.Length && (s[sIndex++] == p[i] || p[i] == '.'));
+                    break;
                 }
-                else if (p[i] == '.')
+                else
                 {
-                    if (i < p.Length - 1 && p[i + 1] == '*')
-                    {
-                        if (i < p.Length - 2)
-                        {
-                            while (s[sIndex] == s[i])
-                            {
-                                if (IsMatch(s.Substring(sIndex++), p.Substring(i + 2)))
-                                {
-                                    return true;
-                                }
-                            }
-                            return false;
-                        }
-                        else
-                        {
-                            return true;
-                        }
-                    }
-                    else
-                    {
-                        if(++sIndex > s.Length && i + 1 < p.Length) return false;
-                    }
+                    if (sIndex >= s.Length || (p[i] != '.' && s[sIndex] != p[i])) return false;
+                    sIndex++;
                 }
-
             }
 
-            return sIndex == s.Length;
+            return p == s || (sIndex == s.Length && (p.Length == i || (p.Length - i == 2 && p[i + 1] == '*')));
         }
+
+            public bool IsMatch(string s, string p, Dictionary<string, bool> memo)
+            {
+                string key = s +"_"+ p;
+
+                if (memo.ContainsKey(key))
+                {
+                    return memo[key];
+                }
+                if (s == p || (s.Length == 1 && p == "."))
+                {
+                    memo[key] = true;
+                    return true;
+                };
+
+
+
+                int sIndex = 0;
+                int i = 0;
+                for (i = 0; i < p.Length; i++)
+                {
+                    if (i < p.Length - 1 && p[i + 1] == '*')
+                    {
+                        do
+                        {
+                            if (sIndex <= s.Length && IsMatch(s.Substring(sIndex), p.Substring(i + 2), memo))
+                            {
+                                memo[key] = true;
+                                return true;
+                            }
+                        }
+                        while (sIndex < s.Length && (s[sIndex++] == p[i] || p[i] == '.'));
+                        break;
+                    }
+                    else
+                    {
+                        if (sIndex >= s.Length || (p[i] != '.' && s[sIndex] != p[i]))
+                        {
+                            memo[key] = false;
+                            return false;
+                        } 
+                        sIndex++;
+                    }
+                }
+
+                memo[key] = p == s || (sIndex == s.Length && (p.Length == i || (p.Length - i == 2 && p[i + 1] == '*')));
+                return memo[key];
+
+            }
     }
 }
 
